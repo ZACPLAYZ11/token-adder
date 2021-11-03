cad9cff59da2c91e6ed2f25ce454882bb6b1c09a
joe <you@example.com>
2021-11-03 02:18:49 +0100

    minimal,  loginThenAddToken doesn't work because redirect remove text input value

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import {
-  createSolidDataset,
-  createThing,
   setThing,
-  addUrl,
   addStringNoLocale,
   saveSolidDatasetAt,
   getSolidDataset,
-  getThingAll,
   getThing,
-  getStringNoLocale,
-  removeThing,
-  FetchError
+  
 } from "@inrupt/solid-client";
 
 import {
@@ -20,18 +14,14 @@ import {
   fetch
 } from "@inrupt/solid-client-authn-browser";
 
-import { SCHEMA_INRUPT, RDF, AS } from "@inrupt/vocab-common-rdf";
-
 const buttonLogin = document.querySelector("#btnLogin");
-const buttonAddToken = document.querySelector("#btnAddToken");
-const labelCreateStatus = document.querySelector("#labelCreateStatus");
+
 // 1a. Start Login Process. Call login() function.
 function startLogin() {
+  let oidcIssuer = document.getElementById('oidcIssuer').value
   return login({
-
     // oidcIssuer: "https://broker.pod.inrupt.com",
-    oidcIssuer: "https://solidcommunity.net",
-
+    oidcIssuer: oidcIssuer,
     redirectUrl: window.location.href,
     clientName: "Getting started !"
   });
@@ -41,122 +31,60 @@ function startLogin() {
 // When redirected after login, call handleIncomingRedirect() function to
 // finish the login process by retrieving session information.
 async function finishLogin() {
+    let token = document.getElementById("tokenValue").value;
     await handleIncomingRedirect();
     const session = getDefaultSession();
-    let podUrl ;
-    let webIdUrl
     if (session.info.isLoggedIn) {
-      // Update the page with the status.
-      document.getElementById("labelStatus").textContent = `Logged in with WebID ${session.info.webId}`;
-      document.getElementById("labelStatus").setAttribute("role", "alert");
-      webIdUrl = session.info.webId;
-      podUrl = webIdUrl.replace('profile/card#me', '');
-      document.getElementById("WebIdURL").setAttribute("value", webIdUrl);
-      // Enable Create button
+      console.log( session)
+      let webIdUrl = session.info.webId;
+      // console.log("webid: " + webIdUrl)
+      // addToken(token, webIdUrl);
     }
-    let d = new Date()
-    addToken(d.getSeconds(), webIdUrl);
+
 }
 
 // The example has the login redirect back to the index.html.
 // finishLogin() calls the function to process login information.
 // If the function is called when not part of the login redirect, the function is a no-op.
+
 finishLogin();
 
 async function addToken(token_value, podUrl) {
-  // labelCreateStatus.textContent = "";
- 
-  // For simplicity and brevity, this tutorial hardcodes the SolidDataset URL.
-  // In practice, you should add a link to this resource in your profile that applications can follow.
+  
+  console.log("podUrl: " + podUrl)
   const profileCardUrl = `${podUrl}/profile/card/`;
+  console.log("pc: " + profileCardUrl)
   const OIDC_SCHEMA ="http://www.w3.org/ns/solid/terms#oidcIssuerRegistrationToken"
   let myProfileCard ;
  
-  // Fetch or create a new reading list.
 
   try {
-    // Attempt to fetch the reading list in case it already exists.
     myProfileCard = await getSolidDataset(profileCardUrl, { fetch: fetch });
-    console.log("could read profile card")
-
-    // Clear the list to override the whole list
-    // let titles = getThingAll(myProfileCard);
   } catch (error) {
-    if (typeof error.statusCode === "number" && error.statusCode === 404) {
-      // if not found, create a new SolidDataset (i.e., the reading list)
-      myProfileCard = createSolidDataset();
-    console.log("could NOT read profile card")
-    console.log("creating one..")
-    } else {
       console.error(error.message);
-    }
   }
 
-    const webIdUrl = myProfileCard.internal_resourceInfo.sourceIri + '#me'
-    let card = getThing(myProfileCard, webIdUrl)
-    card = addStringNoLocale(card, OIDC_SCHEMA, token_value);
-    myProfileCard = setThing(myProfileCard, card);
+  const webIdUrl = myProfileCard.internal_resourceInfo.sourceIri + '#me'
+  let card = getThing(myProfileCard, webIdUrl)
+  card = addStringNoLocale(card, OIDC_SCHEMA, token_value);
+  myProfileCard = setThing(myProfileCard, card);
 
   try {
-     
-    // Save the SolidDataset 
-    let savedReadingList = await saveSolidDatasetAt(
+    // Save the SolidDataset
+    console.log("wtf? " + profileCardUrl)
+    await saveSolidDatasetAt(
       profileCardUrl,
       myProfileCard,
       { fetch: fetch }
     );
-
-    // labelCreateStatus.textContent = "Saved";
-
   } catch (error) {
     console.log(error);
-    // labelCreateStatus.textContent = "Error" + error;
-    // labelCreateStatus.setAttribute("role", "alert");
   } 
 }
 
-
-
-
-async function addIndexToken() {
-  console.log("add token")
-  labelCreateStatus.textContent = "";
-  const podUrl = document.getElementById("WebIdURL").value;
-  const token_value = document.getElementById("token").value;
-  addToken(token_value, podUrl)
-
-}
-
-
-async function fetch_oidc_token() {
-
-    // Refetch the Reading List
-    let savedReadingList = await getSolidDataset(
-      profileCardUrl,
-      { fetch: fetch }
-    );
-
-    let items = getThingAll(savedReadingList);
-
-    let listcontent="";
-    for (let i = 0; i < items.length; i++) {
-      let item = getStringNoLocale(items[i], SCHEMA_INRUPT.name);
-      if (item !== null) {
-          listcontent += item + "\n";
-      }
-    }
-
-    document.getElementById("savedtitles").value = listcontent;
-}
-
-
-buttonLogin.onclick = function() {  
+buttonLogin.onclick = function() {
   startLogin();
 };
 
 
-
-buttonAddToken.onclick = function() {
-  addIndexToken();
-};
 
